9af6293cc3bc69fc78d60db8027f9d086f0e2602
Mito <maxkss37j@gmail.com>
2025-09-26 10:43:07 -0400

    Revisión de test completada

--- src/core/users/users.service.spec.ts
+++ src/core/users/users.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AuditService } from '../audit/audit.service';
+import { EncryptionHelper } from '../auth/helpers/encryption.helper';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+        {
+          provide: AuditService,
+          useValue: {
+            log: jest.fn(),
+          },
+        },
+        {
+          provide: EncryptionHelper,
+          useValue: {
+            encriptar: jest.fn(),
+            desEncriptar: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
